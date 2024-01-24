5bac63307287f62e785a3c8132df3ac7c6960620
giorgiadeieso <157354366+giorgiadeieso@users.noreply.github.com>
2024-01-24 20:28:05 +0000

    .

--- cartella33.c
+++ cartella33.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    int sequenza
+   int x;
+   int n;
+   int s;
+   float m;
+   n = 0;
+   s = 0;
+   m = 0;
+   printf ("inserire un numero\n");
+   scanf ("%d", &x);
+   while ( x >= 0)
+   {
+    n = n + 1;
+    s = s + x;
+    m = s / n;
+    printf ("la media: %f\n", m);
+    printf ("inserire un numero: ");
+    scanf ("%d", &x);
+   }
+   printf ("errore\n");
+}
