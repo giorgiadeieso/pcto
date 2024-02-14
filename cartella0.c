e5a03249fb41a8e1e8de76c0c54aac124d9bd4d4
giorgiadeieso <157354366+giorgiadeieso@users.noreply.github.com>
2024-02-14 20:51:40 +0000

    .

--- cartella0.c
+++ cartella0.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-
 int main()
 {
-    int n;
-    float x;
-    int y;
-    int pippo;
-    n = 3;
-    x = 3.4;
-    y= 3.6;
-    pippo = 5;
-    printf ("n: %d x: %f y = %d \n" , n, x, y);
+   float x;
+   float n;
+   float s;
+   float m;
+   n = 0;
+   s = 0;
+   m = 0;
+   printf ("inserire un numero\n");
+   scanf ("%f", &x);
+   while ( x >= 0)
+   {
+    n = n + 1;
+    s = s + x;
+    m = s / n;
+    printf ("la media: %f\n", m);
+    printf ("inserire un numero: ");
+    scanf ("%f", &x);
+   }
+   printf ("errore\n");
 }
